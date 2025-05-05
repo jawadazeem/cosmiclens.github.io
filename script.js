e7a1de4bdd5a6a1fc9ad04221716e0f5007c53df
Jawad Azeem <jawad.azeem@outlook.com>
2025-05-05 09:20:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Optional: Add smooth scroll or other interactivity
+document.querySelector('.cta-button').addEventListener('click', function() {
+    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
+  });
+  
